e3bfe9c38be8313f4993a6b143b4fe4125bba7f9
Artem Tulupov <artemtu94@gmail.com>
2023-11-25 19:18:37 +0400

    feature: set up ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
-import logo from '../../logo.svg';
-import '../../css/index.css';
+import logo from "../../../src/logo.svg";
+import "../../css/index.css";
 
 function App() {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,55 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: { main: "./src/index.tsx" },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
 
-    open: true
+    open: true,
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: '/node_modules/'
-    },
-    {
-      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-      type: 'asset/resource'
-    },
-    {
-      test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, {
-        loader: 'css-loader',
-        options: { importLoaders: 1 }
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
-        'postcss-loader']
-    },
-    ]
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: "./public/index.html",
     }),
     // new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-  ]
-}
+  ],
+};
