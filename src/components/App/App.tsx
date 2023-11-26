d16e4af806c7be09f724ac018a4bb2e1c8c0c47b
Liya Vysotskaya <Imredrat@yandex.ru>
2023-11-26 21:15:33 +0300

    feat: create Footer proto

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import '../../assets/fonts/fonts.scss';
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Footer />
     </div>
   );
 }
