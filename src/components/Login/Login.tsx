4519d0c94a992af845dc4e30681a05d091b7ee39
Artem Tulupov <artemtu94@gmail.com>
2023-11-27 15:54:57 +0400

    feature: login with styles, implement in App

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./Login.module.scss";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+
+const Login: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <section className={styles.loginSection}>
+      <h3 className={styles.loginText}>Пожалуйста, авторизуйтесь</h3>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            { required: true, message: "Please input your Email!" },
+            { type: "email", message: "Пожалуйста, введите корректный Email" },
+          ]}
+        >
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            placeholder="Введите Email"
+            className={styles.inputs}
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Пожалуйста, введите пароль" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Введите пароль"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Запомнить меня на этом компьютере</Checkbox>
+          </Form.Item>
+        </Form.Item>
+
+        <Form.Item className={styles.buttonContainer}>
+          <Button type="primary" htmlType="submit" className={styles.button}>
+            Войти
+          </Button>
+        </Form.Item>
+        <div className={styles.actionLinksContainer}>
+          <a href="#">Сбросить пароль</a>
+          <a href="#">Регистрация</a>
+        </div>
+      </Form>
+    </section>
+  );
+};
+
+export default Login;
