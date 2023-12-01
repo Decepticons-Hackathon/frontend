d80e8a9f93a557742c3593b76c737658dc9e16ff
Liya Vysotskaya <Imredrat@yandex.ru>
2023-12-01 19:06:13 +0300

    fix: add constant columns

--- src/api/models/ProductMatchedListResult.tsx
+++ src/api/models/ProductMatchedListResult.tsx
@@ -1,16 +1,3 @@
 export type ProductMatchedListResult = {
-  product_id: number;
-  article: string;
-  ean_13: string;
-  name: string;
-  cost: number;
-  min_rec_price: number;
-  rec_price: number;
-  category_id: number;
-  ozon_name: string;
-  name_1c: string;
-  wb_name: string;
-  ozon_article: string;
-  wb_article: string;
-  ym_article: string;
+
 };
--- src/components/ResultTable/ResultTable.tsx
+++ src/components/ResultTable/ResultTable.tsx
@@ -1,180 +1,9 @@
-import React, { useState } from "react";
-import type { TableProps } from "antd";
-import { Button, Space, Table } from "antd";
-import type {
-  ColumnsType,
-  FilterValue,
-  SorterResult,
-} from "antd/es/table/interface";
-import TableHelper from "../TableHelper/TableHelper";
-
-interface DataType {
-  key: string;
-  name: string;
-  dealer: string;
-  product: string;
-  date: number;
-  price: number;
-  dealerPrice: number;
-  status: string;
-}
-
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "OZON",
-    dealer: 'Castorama',
-    product: "Антисептик",
-    date: 1,
-    price: 10,
-    dealerPrice: 12,
-    status: 'Ok',
-  },
-  {
-    key: "2",
-    name: "Wildberries",
-    dealer: 'Castorama',
-    product: "Краска",
-    date: 3,
-    price: 20,
-    dealerPrice: 22,
-    status: 'Postponed',
-  },
-  {
-    key: "3",
-    name: "Yandex Market",
-    dealer: 'Castorama',
-    product: "Стеклоочиститель",
-    date: 2,
-    price: 30,
-    dealerPrice: 32,
-    status: 'Postponed',
-  },
-  {
-    key: "4",
-    name: "Wildberries",
-    dealer: 'Castorama',
-    product: "Антисептик",
-    date: 1,
-    price: 10,
-    dealerPrice: 12,
-    status: 'Ok',
-  },
-  {
-    key: "5",
-    name: "Yandex Market",
-    dealer: 'Castorama',
-    product: "Антисептик",
-    date: 1,
-    price: 10,
-    dealerPrice: 12,
-    status: 'Rejected',
-  },
-  {
-    key: "6",
-    name: "OZON",
-    dealer: 'Ромашка',
-    product: "Антисептик",
-    date: 1,
-    price: 10,
-    dealerPrice: 12,
-    status: 'Rejected',
-  },
-];
+import React from "react";
+import { Space, Table } from "antd";
+import { data } from '../../constants/constants';
+import { columns } from "../../constants/ResultTableColumns";
 
 const ResultTable: React.FC = () => {
-  // const [filteredInfo, setFilteredInfo] = useState<
-  //   Record<string, FilterValue | null>
-  // >({});
-  // const [sortedInfo, setSortedInfo] = useState<SorterResult<DataType>>({});
-
-  // const handleChange: TableProps<DataType>["onChange"] = (
-  //   pagination,
-  //   filters,
-  //   sorter
-  // ) => {
-  //   console.log("Various parameters", pagination, filters, sorter);
-  //   setFilteredInfo(filters);
-  //   setSortedInfo(sorter as SorterResult<DataType>);
-  // };
-
-  // const clearFilters = () => {
-  //   setFilteredInfo({});
-  // };
-
-  // const clearAll = () => {
-  //   setFilteredInfo({});
-  //   setSortedInfo({});
-  // };
-
-  // const setDateSort = () => {
-  //   setSortedInfo({
-  //     order: "descend",
-  //     columnKey: "date",
-  //   });
-  // };
-
-  const columns: ColumnsType<DataType> = [
-    {
-      title: "Product",
-      dataIndex: "product",
-      key: "product",
-      ...TableHelper.getStringListColumnSearchProps("product", data),
-      // sortOrder: sortedInfo.columnKey === "product" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-    {
-      title: "Dealer",
-      dataIndex: "dealer",
-      key: "dealer",
-      ...TableHelper.getStringListColumnSearchProps("dealer", data),
-      sorter: (a, b) => a.dealer.length - b.dealer.length,
-      // sortOrder: sortedInfo.columnKey === "dealer" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-    {
-      title: "Marketplace",
-      dataIndex: "name",
-      key: "name",
-      ...TableHelper.getStringListColumnSearchProps("name", data),
-      sorter: (a, b) => a.name.length - b.name.length,
-      // sortOrder: sortedInfo.columnKey === "name" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-    {
-      title: "Date",
-      dataIndex: "date",
-      key: "date",
-      sorter: (a, b) => a.date- b.date,
-      // sortOrder: sortedInfo.columnKey === "date" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-    {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
-      sorter: (a, b) => a.price- b.price,
-      // sortOrder: sortedInfo.columnKey === "price" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-    {
-      title: "DealerPrice",
-      dataIndex: "dealerPrice",
-      key: "dealerPrice",
-      sorter: (a, b) => a.dealerPrice- b.dealerPrice,
-      // sortOrder: sortedInfo.columnKey === "dealerPrice" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      key: "status",
-      ...TableHelper.getStringListColumnSearchProps("status", data),
-      sorter: (a, b) => a.status.length - b.status.length,
-      // sortOrder: sortedInfo.columnKey === "status" ? sortedInfo.order : null,
-      ellipsis: true,
-    },
-  ];
 
   return (
     <>
@@ -183,7 +12,12 @@ const ResultTable: React.FC = () => {
         <Button onClick={clearFilters}>Clear filters</Button>
         <Button onClick={clearAll}>Clear filters and sorters</Button> */}
       </Space>
-      <Table columns={columns} dataSource={data} size='small' bordered />
+      <Table
+        columns={columns(data.products)}
+        dataSource={data.products}
+        size='small'
+        scroll={{x: 'max-content'}}
+        bordered />
     </>
   );
 };
