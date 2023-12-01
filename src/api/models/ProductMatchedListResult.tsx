bfe4e339734e86dc214a63e2393a571478eba0d8
Liya Vysotskaya <Imredrat@yandex.ru>
2023-12-01 14:09:03 +0300

    fix: add fields for dealers, products, results

--- src/api/models/ProductMatchedListResult.tsx
+++ src/api/models/ProductMatchedListResult.tsx
@@ -1,3 +1,16 @@
 export type ProductMatchedListResult = {
-
+  product_id: number;
+  article: string;
+  ean_13: string;
+  name: string;
+  cost: number;
+  min_rec_price: number;
+  rec_price: number;
+  category_id: number;
+  ozon_name: string;
+  name_1c: string;
+  wb_name: string;
+  ozon_article: string;
+  wb_article: string;
+  ym_article: string;
 };
