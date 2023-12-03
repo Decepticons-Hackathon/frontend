7d70844470377ba7ae87cafc01f3bd02fd9f5611
Artem Tulupov <artemtu94@gmail.com>
2023-12-03 15:46:37 +0400

    feature: useState for pars data, approve func

--- src/components/Matching/Matching.tsx
+++ src/components/Matching/Matching.tsx
@@ -5,7 +5,6 @@ import AutoSearch from "../AutoSearch/AutoSearch";
 import { useState } from "react";
 import RecommendationsTable from "../MatchingTable/RecommendationsTable";
 import UploadGoodsTable from "../UploadGoodsTable/UploadGoodsTable";
-import { parsingData } from "../UploadGoodsTable/UploadGoodsTable";
 
 export const productsMap: any = {};
 
@@ -35,9 +34,165 @@ const recommendations: reccomendstionsType[] = [
   },
 ];
 
+export type ParsingType = {
+  key: string;
+  product_name: string;
+  price: number;
+  product_url: string;
+  date: string;
+};
+
+export const parsing: ParsingType[] = [
+  {
+    key: "1",
+    product_name: "Санитайзер мультиактивный, 1 л",
+    price: 500,
+    product_url: "https://www.ozon.ru/product/antiseptik-1-l-530024221/",
+    date: "2023/12/1",
+  },
+  {
+    key: "2",
+    product_name: "Чистящее средство для окон, 1 л",
+    price: 450,
+    product_url:
+      "https://www.ozon.ru/product/chistyashchee-sredstvo-dlya-okon-1-l-534530024222/",
+    date: "2023/12/2",
+  },
+  {
+    key: "3",
+    product_name: "Универсальный очиститель, 5 л",
+    price: 750,
+    product_url:
+      "https://www.ozon.ru/product/universalnyy-ochistitel-5-l-535430024223/",
+    date: "2023/12/3",
+  },
+  {
+    key: "4",
+    product_name: "Средство для мытья посуды, 500 мл",
+    price: 300,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-mytya-posudy-500-ml-53230024224/",
+    date: "2023/12/4",
+  },
+  {
+    key: "5",
+    product_name: "Порошок для стирки, 2 кг",
+    price: 600,
+    product_url:
+      "https://www.ozon.ru/product/poroshok-dlya-stirki-2-kg-53002234225/",
+    date: "2023/12/5",
+  },
+  {
+    key: "6",
+    product_name: "Ополаскиватель для белья, 1 л",
+    price: 350,
+    product_url:
+      "https://www.ozon.ru/product/opolaskivatel-dlya-belya-1-l-53002422336/",
+    date: "2023/12/6",
+  },
+  {
+    key: "7",
+    product_name: "Чистящее средство для ванны, 750 мл",
+    price: 400,
+    product_url:
+      "https://www.ozon.ru/product/chistyashchee-sredstvo-dlya-vanny-750-ml-53002422744/",
+    date: "2023/12/7",
+  },
+  {
+    key: "8",
+    product_name: "Средство для чистки ковров, 500 мл",
+    price: 10000,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002434228/",
+    date: "2023/12/8",
+  },
+  {
+    key: "9",
+    product_name: "Дезинфицирующий спрей",
+    price: 5,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002324228/",
+    date: "2023/12/8",
+  },
+  {
+    key: "10",
+    product_name: "Средство для удаления пятен",
+    price: 3,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002412228/",
+    date: "2023/12/8",
+  },
+  {
+    key: "11",
+    product_name: "Жидкость для мытья полов",
+    price: 120,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5310024228/",
+    date: "2023/12/8",
+  },
+  {
+    key: "12",
+    product_name: "Средство для чистки туалета",
+    price: 50,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5300524228/",
+    date: "2023/12/8",
+  },
+  {
+    key: "13",
+    product_name: "Освежитель воздухан",
+    price: 1110,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-530024253428/",
+    date: "2023/12/8",
+  },
+  {
+    key: "14",
+    product_name: "Средство от налета и ржавчины",
+    price: 11500,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-530024228454/",
+    date: "2023/12/8",
+  },
+  {
+    key: "15",
+    product_name: "Антибактериальное мыло",
+    price: 2500,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002422844/",
+    date: "2023/12/8",
+  },
+  {
+    key: "16",
+    product_name: "Чистящие салфетки",
+    price: 3500,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5300242458/",
+    date: "2023/12/8",
+  },
+  {
+    key: "17",
+    product_name: "Средство для уборки кухни",
+    price: 1400,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-530024276/",
+    date: "2023/12/8",
+  },
+  {
+    key: "19",
+    product_name: "Жидкое быдло 2 литра кек",
+    price: 5250,
+    product_url:
+      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5300242558/",
+    date: "2023/12/8",
+  },
+];
+
 const Matching: React.FC = () => {
   const [isBtnsActive, setIsBtnsActive] = useState(false);
   const [recommendationsData, setRecommendationsData] = useState([]);
+  const [parsingData, setParsingData] = useState<ParsingType[]>(parsing);
+
   const [selectedLineUploadGoods, setSelectedLineUploadGoods] = useState<
     string | null
   >(null);
@@ -68,6 +223,7 @@ const Matching: React.FC = () => {
 
   const approveClick: any = () => {
     const selectedUploadGoodsItem = parsingData.find(
+      //@ts-ignore
       (item) => item.key === selectedLineUploadGoods
     );
     const selectedRecommendationItem = recommendationsData.find(
@@ -79,6 +235,15 @@ const Matching: React.FC = () => {
         uploadGoods: selectedUploadGoodsItem,
         recommendation: selectedRecommendationItem,
       };
+
+      const updatedParsingData = parsingData.filter(
+        //@ts-ignore
+        (item) => item.key !== selectedLineUploadGoods
+      );
+
+      //@ts-ignore
+      setParsingData(updatedParsingData);
+      console.log(updatedParsingData);
 
       //@ts-ignore
       setApprovedItems((prevItems) => [...prevItems, newItem]);
@@ -97,6 +262,7 @@ const Matching: React.FC = () => {
           randomRecommenadtions={onGoodsTableRowClick}
           onUploadSelectClick={setSelectedLineUploadGoods}
           selectedLineUploadGoods={selectedLineUploadGoods}
+          parsingData={parsingData}
         />
       </div>
       <div className={styles.optionsContainer}>
--- src/components/UploadGoodsTable/UploadGoodsTable.tsx
+++ src/components/UploadGoodsTable/UploadGoodsTable.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import type { TableProps } from "antd";
 import { Button, Space, Table } from "antd";
 import styles from "./UploadGoodsTable.module.scss";
+import { ParsingType } from "../Matching/Matching";
 
 import type {
   ColumnsType,
@@ -10,165 +11,15 @@ import type {
 } from "antd/es/table/interface";
 import TableHelper from "./HelperTable";
 
-type ParsingType = {
-  key: string;
-  product_name: string;
-  price: number;
-  product_url: string;
-  date: string;
-};
-
-export const parsingData: ParsingType[] = [
-  {
-    key: "1",
-    product_name: "Санитайзер мультиактивный, 1 л",
-    price: 500,
-    product_url: "https://www.ozon.ru/product/antiseptik-1-l-530024221/",
-    date: "2023/12/1",
-  },
-  {
-    key: "2",
-    product_name: "Чистящее средство для окон, 1 л",
-    price: 450,
-    product_url:
-      "https://www.ozon.ru/product/chistyashchee-sredstvo-dlya-okon-1-l-534530024222/",
-    date: "2023/12/2",
-  },
-  {
-    key: "3",
-    product_name: "Универсальный очиститель, 5 л",
-    price: 750,
-    product_url:
-      "https://www.ozon.ru/product/universalnyy-ochistitel-5-l-535430024223/",
-    date: "2023/12/3",
-  },
-  {
-    key: "4",
-    product_name: "Средство для мытья посуды, 500 мл",
-    price: 300,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-mytya-posudy-500-ml-53230024224/",
-    date: "2023/12/4",
-  },
-  {
-    key: "5",
-    product_name: "Порошок для стирки, 2 кг",
-    price: 600,
-    product_url:
-      "https://www.ozon.ru/product/poroshok-dlya-stirki-2-kg-53002234225/",
-    date: "2023/12/5",
-  },
-  {
-    key: "6",
-    product_name: "Ополаскиватель для белья, 1 л",
-    price: 350,
-    product_url:
-      "https://www.ozon.ru/product/opolaskivatel-dlya-belya-1-l-53002422336/",
-    date: "2023/12/6",
-  },
-  {
-    key: "7",
-    product_name: "Чистящее средство для ванны, 750 мл",
-    price: 400,
-    product_url:
-      "https://www.ozon.ru/product/chistyashchee-sredstvo-dlya-vanny-750-ml-53002422744/",
-    date: "2023/12/7",
-  },
-  {
-    key: "8",
-    product_name: "Средство для чистки ковров, 500 мл",
-    price: 10000,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002434228/",
-    date: "2023/12/8",
-  },
-  {
-    key: "9",
-    product_name: "Дезинфицирующий спрей",
-    price: 5,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002324228/",
-    date: "2023/12/8",
-  },
-  {
-    key: "10",
-    product_name: "Средство для удаления пятен",
-    price: 3,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002412228/",
-    date: "2023/12/8",
-  },
-  {
-    key: "11",
-    product_name: "Жидкость для мытья полов",
-    price: 120,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5310024228/",
-    date: "2023/12/8",
-  },
-  {
-    key: "12",
-    product_name: "Средство для чистки туалета",
-    price: 50,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5300524228/",
-    date: "2023/12/8",
-  },
-  {
-    key: "13",
-    product_name: "Освежитель воздухан",
-    price: 1110,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-530024253428/",
-    date: "2023/12/8",
-  },
-  {
-    key: "14",
-    product_name: "Средство от налета и ржавчины",
-    price: 11500,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-530024228454/",
-    date: "2023/12/8",
-  },
-  {
-    key: "15",
-    product_name: "Антибактериальное мыло",
-    price: 2500,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-53002422844/",
-    date: "2023/12/8",
-  },
-  {
-    key: "16",
-    product_name: "Чистящие салфетки",
-    price: 3500,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5300242458/",
-    date: "2023/12/8",
-  },
-  {
-    key: "17",
-    product_name: "Средство для уборки кухни",
-    price: 1400,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-530024276/",
-    date: "2023/12/8",
-  },
-  {
-    key: "19",
-    product_name: "Жидкое быдло 2 литра кек",
-    price: 5250,
-    product_url:
-      "https://www.ozon.ru/product/sredstvo-dlya-chistki-kovrov-500-ml-5300242558/",
-    date: "2023/12/8",
-  },
-];
-
-const UploadGoodsTable: React.FC<{
+type UploadGoodsTableProps = {
+  parsingData: ParsingType[];
   randomRecommenadtions: () => void;
   onUploadSelectClick: (key: string) => void;
   selectedLineUploadGoods: string | null;
-}> = ({
+};
+
+const UploadGoodsTable: React.FC<UploadGoodsTableProps> = ({
+  parsingData,
   randomRecommenadtions,
   onUploadSelectClick,
   selectedLineUploadGoods,
